0fbafd7a2355b0a16d0bd64287493ff4bff344d4
BrandonWedu <Brandon.education@proton.me>
2024-11-16 12:23:22 -0500

    Added Address Data Class

---
+++ Project4/Models/Address.cs
@@ -0,0 +1,44 @@
+﻿namespace Project4.Models
+{
+    //Contains data for an Address
+    public class Address
+    {
+        private string street;
+        private string city;
+        private string state;
+        private string zipCode;
+
+        public Address(string street, string city, string state, string zipCode)
+        {
+            this.street = street;
+            this.city = city;
+            this.state = state;
+            this.zipCode = zipCode;
+        }
+        public string Street
+        {
+            get { return street; }
+            set { this.street = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { this.city = value; }
+        }
+        public string State
+        {
+            get { return state; }
+            set { this.state = value; }
+        }
+        public string ZipCode
+        {
+            get { return zipCode; }
+            set { this.zipCode = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{street}, {city}, {state} {ZipCode}";
+        }
+    }
+}
