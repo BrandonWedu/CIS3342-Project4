d62c0e433e7701fed9eee01ea5f76a7d099603e0
BrandonWedu <Brandon.education@proton.me>
2024-11-16 11:59:22 -0500

    added controller classes

---
+++ Project4/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project4.Controllers
+{
+    //Handles Creating an account and logging in
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
