11bd4836ed9da0c47920eade34c53480561e2542
BrandonWedu <Brandon.education@proton.me>
2024-11-18 18:52:13 -0500

    showing view

---
+++ Project4/Models/ViewModels/ShowingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Project4.Models.ViewModels
+{
+    public class ShowingViewModel
+    {
+        public Home Home { get; set; }
+        public Client Client { get; set; }
+        public Showing Showing { get; set; }
+        public ShowingStatus Status { get; set; }
+        public States States { get; set; }
+    }
+}
