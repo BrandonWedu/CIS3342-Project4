f043675b7a25d20d9d12d9dcba234c776dba9050
tur31103 <tur31103@temple.edu>
2024-11-19 09:44:35 -0500

    started agent and password hashing
    -Password hashing with salt complete
    -Agent class complete

---
+++ Project4/Models/AgentList.cs
@@ -0,0 +1,37 @@
+﻿namespace Project4.Models
+{
+	public class AgentList
+	{
+		private List<Agent> allAgents = new List<Agent>();
+		private PasswordHasher passwordHasher = new PasswordHasher();
+
+		public AgentList()
+		{
+
+		}
+
+		public List<Agent> GetAllAgents()
+		{
+			return allAgents;
+		}
+
+		public Agent GetAgentByUsername(string username)
+		{
+			List<Agent> currentAgent = new List<Agent>() ;
+			foreach (Agent agent in allAgents)
+			{
+				if (agent.AgentUsername == username)
+				{
+					currentAgent.Add(agent);
+				}
+			}
+			return currentAgent[0];
+		}
+
+		public void CreateNewAgent()
+		{
+
+		}
+
+	}
+}
---
+++ Project4/Models/PasswordHasher.cs
@@ -0,0 +1,51 @@
+﻿using Project4.Controllers;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Identity;
+
+namespace Project4.Models
+{
+	public class PasswordHasher
+	{
+		private readonly PasswordHasher<Agent> hasher = new PasswordHasher<Agent>();
+		private AgentList allAgents = new AgentList();
+		private string salt;
+
+
+		public string GenerateSalt()
+		{
+			var randomNumber = RandomNumberGenerator.Create();
+			var saltBytes = new byte[16];
+			randomNumber.GetBytes(saltBytes);
+			salt = Convert.ToBase64String(saltBytes);
+			return Convert.ToBase64String(saltBytes);
+		}
+
+		public string HashPasswordWithSalt(string password, string salt)
+		{
+			string saltedPassword = password + salt;
+			return hasher.HashPassword(null, saltedPassword);
+		}
+
+		public string GetSalt()
+		{
+			return salt;
+		}
+
+		public bool VerifyPassword(string username, string enteredPassword)
+		{
+			Agent currentAgent = allAgents.GetAgentByUsername(username);
+
+			if (currentAgent == null)
+			{
+				return false;
+			}
+			else
+			{
+				string saltedPassword = enteredPassword + currentAgent.AgentSalt;
+				var result = hasher.VerifyHashedPassword(null, currentAgent.AgentPassword, saltedPassword);
+				return result == PasswordVerificationResult.Success;
+			}
+
+		}
+	}
+}
