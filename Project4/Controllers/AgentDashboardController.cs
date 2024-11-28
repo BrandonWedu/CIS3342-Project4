00062d4cd3cd018bd460a0d6a8c9198a50057260
BrandonWedu <Brandon.education@proton.me>
2024-11-28 03:04:53 -0500

    Added Delete Cookie to the Agent Dashboard

--- Project4/Controllers/AgentDashboardController.cs
+++ Project4/Controllers/AgentDashboardController.cs
@@ -8,5 +8,16 @@ namespace Project4.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult DeleteCookie()
+        {
+            CookieOptions agentCookieOptions = new CookieOptions();
+            //Add a new cookie with the same name and set it to expire yesterday
+            agentCookieOptions.Expires = DateTime.Now.AddDays(-1);
+            HttpContext.Response.Cookies.Append("LoggedInAgent", "", agentCookieOptions);
+
+            return View("AgentDashboard");
+        }
+
     }
 }
