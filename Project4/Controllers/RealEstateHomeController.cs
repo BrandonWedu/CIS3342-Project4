4cff6c82be7d73e9210ce524eee9a8c08a72c56a
BrandonWedu <Brandon.education@proton.me>
2024-11-30 15:07:55 -0500

    breakthrough barely implemented

--- Project4/Controllers/RealEstateHomeController.cs
+++ Project4/Controllers/RealEstateHomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.IdentityModel.Tokens;
 using Project4.Models;
 using System.Text.Json;
 
@@ -15,41 +16,54 @@ namespace Project4.Controllers
             {
                 return View("Dashboard");
             }
-            string agentJson = HttpContext.Session.GetString("Agent");
-            Agent agent = JsonSerializer.Deserialize<Agent>(agentJson);
-            Home home = new Home(
-                agent,
-                0,
-                new Address("", "", States.Alabama, ""),
-                PropertyType.SingleFamily,
-                DateTime.Now.Year,
-                GarageType.SingleCar,
-                "",
-                DateTime.Now, 
-                SaleStatus.OffMarket, 
-                new Images(),
-                new Amenities(), 
-                new TemperatureControl(HeatingTypes.CentralHeating, CoolingTypes.CentralAir),
-                new Rooms(), 
-                new Utilities()
-                );
-            //TempData["Home"] = home;
-            return View(home);
-        }
-        [HttpPost]
-        public IActionResult CreateHome(Home home)
-        {
-            if (HttpContext.Session.GetString("Agent") != null)
-            {
-                return View("Dashboard");
-            }
-            TempData["Home"] = home;   
-            return View(home);
-        }
-        [HttpPost]
-        public IActionResult AddHome(Home home) {
-
+            //string agentJson = HttpContext.Session.GetString("Agent");
+            //Agent agent = JsonSerializer.Deserialize<Agent>(agentJson);
+            //Home home = new Home(
+                //agent,
+                //0,
+                //new Address("", "", States.Alabama, ""),
+                //PropertyType.SingleFamily,
+                //DateTime.Now.Year,
+                //GarageType.SingleCar,
+                //"",
+                //DateTime.Now, 
+                //SaleStatus.OffMarket, 
+                //new Images(),
+                //new Amenities(), 
+                //new TemperatureControl(HeatingTypes.CentralHeating, CoolingTypes.CentralAir),
+                //new Rooms(), 
+                //new Utilities()
+                //);
             return View();
+        }
+       
+        [HttpPost]
+        public IActionResult HomeForm(string button)
+        {
+            switch (button)
+            {
+                case "AddRoom":
+                    AddRoom();
+                    break;
+            }
+            return View("CreateHome");
+        }
+        public void AddRoom() 
+        {
+            if (TempData["RoomCount"] != null)
+            {
+                TempData["RoomCount"] = (int)TempData["RoomCount"] + 1;
+            } 
+            else
+            {
+                TempData["RoomCount"] = 1;
+            }
+            //save request.form to temp data
+            foreach (string key in Request.Form.Keys)
+            {
+                    TempData[key] = Request.Form[key];
+            }
+            TempData.Keep();
         }
     }
 }
